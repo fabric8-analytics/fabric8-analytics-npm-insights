dea74bd84b72e4a595632e912e67b2564da7f87f
Avishkar Gupta <avgupta@redhat.com>
2018-03-26 22:18:09 +0530

    Enhanced API specification

--- recommendation_engine/flask_predict.py
+++ recommendation_engine/flask_predict.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import flask
 from flask import Flask, request, json
 from predictor.online_recommendation import PMFRecommendation
-import os
 
 app = Flask(__name__)
 
 rec = PMFRecommendation(10)
 
-@app.route('/', methods=['GET', 'POST'])
-def predict():
+
+@app.route('/api/v1/liveness', methods=['GET'])
+def liveness():
+    return flask.jsonify({}), 200
+
+
+@app.route('/api/v1/readiness', methods=['GET'])
+def readiness():
+    return flask.jsonify({"status": "ready"}), 200
+
+
+@app.route('/api/v1/companion_recommendation', methods=['POST'])
+def recommendation():
     global rec
-    if request.method == 'GET':
-        return json.dumps({"Status": "ok"})
+    recommendations = rec.predict(request.json['stack'])
+    if recommendations:
+        return flask.jsonify(recommendations), 200
     else:
-        print(request.json)
-        return json.dumps(rec.predict(request.json['stack']))
+        return flask.jsonify({"status": "error",
+                              "response": "Could not recommend packages"}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
