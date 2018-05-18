4ff694b4c4cf41af75ca49bb2aee52b9cc911994
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-18 15:39:41 +0200

    Run all checks

--- tests/scripts/entrypoint-test.sh
+++ tests/scripts/entrypoint-test.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+echo "*****************************************"
+echo "*** Cyclomatic complexity measurement ***"
+echo "*****************************************"
+radon cc -s -a -i venv .
+
+echo "*****************************************"
+echo "*** Maintainability Index measurement ***"
+echo "*****************************************"
+radon mi -s -i venv .
+
+echo "*****************************************"
+echo "*** Unit tests ***"
+echo "*****************************************"
+
 pytest /tests/unit_tests/
