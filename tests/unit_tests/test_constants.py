21262ea779cc4a10423c8d0a8bbb71d49dcae475
Avishkar Gupta <avgupta@redhat.com>
2018-06-01 09:40:20 +0530

    Add test to see constant export succeeds

---
+++ tests/unit_tests/test_constants.py
@@ -0,0 +1,16 @@
+import unittest
+
+from recommendation_engine.config.cloud_constants import *
+from recommendation_engine.config.path_constants import *
+
+
+class TestImport(unittest.TestCase):
+    """Test if import goes well."""
+
+    def test_cloud_constants(self):
+        """Test if able to import cloud constants."""
+        pass
+
+    def test_path_constants(self):
+        """Test if able to import path constants."""
+        pass
