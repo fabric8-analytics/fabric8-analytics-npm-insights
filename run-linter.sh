ccea9e441fa7a066efb7195a6159b83f77cdc63a
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-22 10:33:41 +0200

    Use proper quoting in scripts to run linters/checkers

--- run-linter.sh
+++ run-linter.sh
@@ -1,19 +1,22 @@
+#!/bin/bash
+
 directories="recommendation_engine deployment tests"
+pass=0
 fail=0
 
 function prepare_venv() {
-    VIRTUALENV=`which virtualenv`
+    VIRTUALENV=$(which virtualenv)
     if [ $? -eq 1 ]; then
         # python34 which is in CentOS does not have virtualenv binary
-        VIRTUALENV=`which virtualenv-3`
+        VIRTUALENV=$(which virtualenv-3)
     fi
 
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 `which pip3` install pycodestyle
+    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install pycodestyle
 }
 
 echo "----------------------------------------------------"
 echo "Running Python linter against following directories:"
-echo $directories
+echo "$directories"
 echo "----------------------------------------------------"
 echo
 
@@ -22,15 +25,16 @@ echo
 # checks for the whole directories
 for directory in $directories
 do
-    files=`find $directory -path $directory/venv -prune -o -name '*.py' -print`
+    files=$(find "$directory" -path "$directory/venv" -prune -o -name '*.py' -print)
 
     for source in $files
     do
-        echo $source
-        pycodestyle $source
+        echo "$source"
+        pycodestyle "$source"
         if [ $? -eq 0 ]
         then
             echo "    Pass"
+            let "pass++"
         else
             echo "    Fail"
             let "fail++"
@@ -41,8 +45,9 @@ done
 
 if [ $fail -eq 0 ]
 then
-    echo "All checks passed"
+    echo "All checks passed for $pass source files"
 else
-    echo "Linter fail, $fail source files need to be fixed"
+    let total=$pass+$fail
+    echo "Linter fail, $fail source files out of $total source files need to be fixed"
     exit 1
 fi
