d7f69ec89ee3cbeaf8c938e93847c831f8674dbd
Avishkar Gupta <avgupta@redhat.com>
2018-09-20 14:30:23 +0530

    Change to production S3 bucket
    Signed-off-by: Avishkar Gupta <avgupta@redhat.com>

--- recommendation_engine/config/path_constants.py
+++ recommendation_engine/config/path_constants.py
@@ -1,10 +1,11 @@
 """Contains the path constants for S3 and local storage."""
+import os
 
-PMF_MODEL_PATH = 'models/cvae-model/pmf-packagedata.mat'
-PACKAGE_TO_ID_MAP = 'node-pmf-scoring/package_to_index_map.json'
-ID_TO_PACKAGE_MAP = 'node-pmf-scoring/index_to_package_map.json'
-PRECOMPUTED_STACKS = 'training-data-node/manifest_user_data.dat'
-TRAINING_DATA_ITEMS = 'training-data-node/packagedata-train-5-items.dat'
-PACKAGE_TAG_MAP = 'node-pmf-scoring/package_tag_map.json'
-SDAE_MODEL_PATH = 'models/sdae-model/'
-CVAE_MODEL_PATH = 'models/cvae-model/'
+ECOSYSTEM = os.environ.get('CHESTER_SCORING_REGION', 'npm')
+PMF_MODEL_PATH = os.path.join(ECOSYSTEM, 'models/cvae-model/pmf-packagedata.mat')
+PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, 'node-pmf-scoring/package_to_index_map.json')
+ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, 'node-pmf-scoring/index_to_package_map.json')
+PRECOMPUTED_STACKS = os.path.join(ECOSYSTEM, 'training-data-node/manifest_user_data.dat')
+TRAINING_DATA_ITEMS = os.path.join(ECOSYSTEM, 'training-data-node/packagedata-train-5-items.dat')
+PACKAGE_TAG_MAP = os.path.join(ECOSYSTEM, 'node-pmf-scoring/package_tag_map.json')
+CVAE_MODEL_PATH = os.path.join(ECOSYSTEM, 'models/cvae-model/')
