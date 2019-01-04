0483c4ad75299a960660d1e3e12025da1c089a10
miteshvp <mitpatel@redhat.com>
2019-01-04 10:24:01 +0530

    introduce model versioning

--- recommendation_engine/config/path_constants.py
+++ recommendation_engine/config/path_constants.py
@@ -2,10 +2,15 @@
 import os
 
 ECOSYSTEM = os.environ.get('CHESTER_SCORING_REGION', 'npm')
-PMF_MODEL_PATH = os.path.join(ECOSYSTEM, 'models/cvae-model/pmf-packagedata.mat')
-PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, 'node-pmf-scoring/package_to_index_map.json')
-ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, 'node-pmf-scoring/index_to_package_map.json')
-PRECOMPUTED_STACKS = os.path.join(ECOSYSTEM, 'training-data-node/manifest_user_data.dat')
-TRAINING_DATA_ITEMS = os.path.join(ECOSYSTEM, 'training-data-node/packagedata-train-5-items.dat')
-PACKAGE_TAG_MAP = os.path.join(ECOSYSTEM, 'node-pmf-scoring/package_tag_map.json')
-CVAE_MODEL_PATH = os.path.join(ECOSYSTEM, 'models/cvae-model/')
+MODEL_VERSION = os.environ.get('MODEL_VERSION', '2019-01-03')
+PMF_MODEL_PATH = os.path.join(ECOSYSTEM, MODEL_VERSION, 'models/cvae-model/pmf-packagedata.mat')
+PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION,
+                                 'node-pmf-scoring/package_to_index_map.json')
+ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION,
+                                 'node-pmf-scoring/index_to_package_map.json')
+PRECOMPUTED_STACKS = os.path.join(ECOSYSTEM, MODEL_VERSION,
+                                  'training-data-node/manifest_user_data.dat')
+TRAINING_DATA_ITEMS = os.path.join(ECOSYSTEM, MODEL_VERSION,
+                                   'training-data-node/packagedata-train-5-items.dat')
+PACKAGE_TAG_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION, 'node-pmf-scoring/package_tag_map.json')
+CVAE_MODEL_PATH = os.path.join(ECOSYSTEM, MODEL_VERSION, 'models/cvae-model/')
