d83f0da7edfe9c5ccb8753bdc0e9e4240607256d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-22 10:22:56 +0100

    Tests for write_json_file function

--- tests/unit_tests/test_s3_data_store.py
+++ tests/unit_tests/test_s3_data_store.py
@@ -3,7 +3,7 @@
 import pytest
 
 from recommendation_engine.data_store.s3_data_store import S3DataStore
-from tests.s3_mocks import MockedS3Resource
+from tests.unit_tests.s3_mocks import MockedS3Resource
 
 
 def test_initial_state():
@@ -31,3 +31,19 @@ def test_read_json_file_negative():
     s3DataStore = S3DataStore("bucket", "access_key", "secret_key")
     with pytest.raises(Exception):
         s3DataStore.read_json_file("file")
+
+
+def test_write_json_file():
+    """Check the method write_json_file()."""
+    # try for improper access_key and secret_key
+    s3DataStore = S3DataStore("bucket", "access_key", "secret_key")
+    with pytest.raises(Exception):
+        s3DataStore.write_json_file("file", {})
+
+
+def test_write_json_file_mocked_s3():
+    """Check the method write_json_file() with mocked S3 object."""
+    # try for improper access_key and secret_key
+    s3DataStore = S3DataStore("bucket", "access_key", "secret_key")
+    s3DataStore.s3_resource = MockedS3Resource()
+    s3DataStore.write_json_file("file", {})
