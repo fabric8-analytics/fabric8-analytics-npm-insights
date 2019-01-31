cc646a310f53af7adcd98b09b00fd31c8864c650
miteshvp <mitpatel@redhat.com>
2019-01-31 09:47:50 +0530

    Introduce proper naming convention for model retraining

--- recommendation_engine/config/path_constants.py
+++ recommendation_engine/config/path_constants.py
@@ -3,14 +3,18 @@ import os
 
 ECOSYSTEM = os.environ.get('CHESTER_SCORING_REGION', 'npm')
 MODEL_VERSION = os.environ.get('MODEL_VERSION', '2019-01-03')
-PMF_MODEL_PATH = os.path.join(ECOSYSTEM, MODEL_VERSION, 'models/cvae-model/pmf-packagedata.mat')
-PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION,
-                                 'node-pmf-scoring/package_to_index_map.json')
-ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION,
-                                 'node-pmf-scoring/index_to_package_map.json')
-PRECOMPUTED_STACKS = os.path.join(ECOSYSTEM, MODEL_VERSION,
-                                  'training-data-node/manifest_user_data.dat')
-TRAINING_DATA_ITEMS = os.path.join(ECOSYSTEM, MODEL_VERSION,
-                                   'training-data-node/packagedata-train-5-items.dat')
-PACKAGE_TAG_MAP = os.path.join(ECOSYSTEM, MODEL_VERSION, 'node-pmf-scoring/package_tag_map.json')
-CVAE_MODEL_PATH = os.path.join(ECOSYSTEM, MODEL_VERSION, 'models/cvae-model/')
+DEPLOYMENT_PREFIX = os.environ.get('DEPLOYMENT_PREFIX', 'dev')
+PMF_MODEL_PATH = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                              'intermediate-model/cvae-model/pmf-packagedata.mat')
+PACKAGE_TO_ID_MAP = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                                 'trained-model/package_to_index_map.json')
+ID_TO_PACKAGE_MAP = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                                 'trained-model/index_to_package_map.json')
+PRECOMPUTED_STACKS = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                                  'data/manifest_user_data.dat')
+TRAINING_DATA_ITEMS = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                                   'data/packagedata-train-5-items.dat')
+PACKAGE_TAG_MAP = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                               'trained-model/package_tag_map.json')
+CVAE_MODEL_PATH = os.path.join(ECOSYSTEM, DEPLOYMENT_PREFIX, MODEL_VERSION,
+                               'intermediate-model/cvae-model/')
