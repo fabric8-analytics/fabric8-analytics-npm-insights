6d744f1b19a3a534e93286576d15da5f39b728f2
Lucky Suman <lsuman@redhat.com>
2019-04-29 12:18:23 +0530

    Ad training datastore files and change the bash files accordingly

---
+++ training/datastore/s3_connection.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This module contains the code that deals with the PMF piece for scoring.
+
+Copyright © 2018 Red Hat Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from rudra.data_store.aws import AmazonS3
+from training.datastore.utils import Utility
+from rudra import logger
+import os
+
+
+class GetData:
+    """This class defines the S3 Connections viz fetching and storing data."""
+
+    def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
+                 aws_bucket_name='cvae-insights',
+                 deployment_prefix='dev', model_version='2019-02-27'):
+        """Create an instance of GetData."""
+        self.aws_access_key_id = os.environ.get('AWS_S3_ACCESS_KEY_ID', '')
+        self.aws_secret_access_key = os.environ.get('AWS_S3_SECRET_ACCESS_KEY', '')
+        self.github_token = os.environ.get('GITHUB_TOKEN', '')
+        self.bucket_name = aws_bucket_name
+        self.deployment_prefix = deployment_prefix
+        self.version_name = model_version
+        self.s3_object = AmazonS3(bucket_name=self.bucket_name,
+                                  aws_access_key_id=self.aws_access_key_id,
+                                  aws_secret_access_key=self.aws_secret_access_key)
+        self.s3_client = self.load_S3()
+        self.utility = Utility()
+
+    def load_S3(self):
+        """Establish the connection with S3."""
+        self.s3_object.connect()
+        if self.s3_object.is_connected():
+            logger.info("S3 connection established.")
+            return self.s3_object
+        else:
+            raise Exception
+
+    def load_raw_data(self):
+        """Load the raw data from S3 bucket."""
+        NPM_raw_data_path = os.path.join("npm", self.deployment_prefix, self.version_name,
+                                         "data/manifest.json")
+        if (self.s3_client.object_exists(NPM_raw_data_path)):
+            try:
+                raw_data_dict_ = self.s3_client.read_json_file(NPM_raw_data_path)
+                logger.info("Size of Raw Manifest file is: {}".format(len(raw_data_dict_)))
+                return raw_data_dict_
+            except Exception:
+                raise Exception
+
+    def load_existing_data(self):
+        """Load the node registry dump from S3 bucket."""
+        NPM_clean_json_data_path = os.path.join("npm", "dev", "2019-01-03",
+                                                "data/node-package-details-with-url.json")
+        if (self.s3_client.object_exists(NPM_clean_json_data_path)):
+            try:
+                logger.info("Path Existed")
+                existing_data = self.s3_client.read_generic_file(NPM_clean_json_data_path)
+                existing_df = self.utility.read_json_file(existing_data)
+                logger.info("Size of Raw df with url is: {}".format(len(existing_df)))
+                return existing_df
+            except Exception:
+                raise Exception("S3 connection error")
+        else:
+            raise ValueError("Given Path is not present.")
