6cb844fcacacb1c32683358862dbfe024572d2fd
Lucky Suman <lsuman@redhat.com>
2019-11-27 09:27:36 +0530

    Updated path according to training function

--- recommendation_engine/config/path_constants.py
+++ recommendation_engine/config/path_constants.py
@@ -1,21 +1,25 @@
 """Contains the path constants for S3 and local storage."""
 import os
 from rudra.data_store.local_data_store import LocalDataStore
-num_users = os.environ.get(num_train)
+num_users = os.environ.get("TRAIN_PER_USER", 5)
 
 MODEL_VERSION = os.environ.get('MODEL_VERSION', '2019-01-03')
 TEMPORARY_PATH = '/tmp/trained-model/'
-TEMPORARY_DATA_PATH='/tmp/data/'
+TEMPORARY_DATA_PATH = '/tmp/data/'
 TEMPORARY_MODEL_PATH = '/tmp/intermediate-model/cvae-model/'
 TEMPORARY_DATASTORE = LocalDataStore('/tmp')
 TEMPORARY_LATENT_PATH = os.path.join(TEMPORARY_MODEL_PATH, 'latent_pretrain_all')
 TEMPORARY_SDAE_PATH = os.path.join(TEMPORARY_MODEL_PATH, 'train')
 TEMPORARY_CVAE_PATH = os.path.join(TEMPORARY_MODEL_PATH, 'cvae')
 TEMPORARY_PMF_PATH = os.path.join(TEMPORARY_MODEL_PATH, 'pmf-packagedata.mat')
-TEMPORARY_USER_ITEM_FILEPATH = os.path.join(TEMPORARY_PATH, "user_train_data.dat")
-TEMPORARY_ITEM_USER_FILEPATH = os.path.join(TEMPORARY_PATH, "item_train_data.dat")
-USER_ITEM_FILEPATH = os.path.join(MODEL_VERSION, 'data', "")
-ITEM_USER_FILEPATH = os.path.join(MODEL_VERSION, 'data', "item_train_data.dat")
+TEMPORARY_USER_ITEM_FILEPATH = os.path.join(TEMPORARY_DATA_PATH,
+                                            "packagedata-train-" + str(num_users) + "-users.dat")
+TEMPORARY_ITEM_USER_FILEPATH = os.path.join(TEMPORARY_DATA_PATH,
+                                            "packagedata-train-" + str(num_users) + "-items.dat")
+USER_ITEM_FILEPATH = os.path.join(MODEL_VERSION, 'data',
+                                  "packagedata-train-" + str(num_users) + "-users.dat")
+ITEM_USER_FILEPATH = os.path.join(MODEL_VERSION, 'data',
+                                  "packagedata-train-" + str(num_users) + "-items.dat")
 PRECOMPUTED_MANIFEST_PATH = os.path.join(MODEL_VERSION, "data/manifest_user_data.dat")
 PMF_MODEL_PATH = os.path.join(MODEL_VERSION, 'intermediate-model/cvae-model', 'pmf-packagedata.mat')
 PACKAGE_TO_ID_MAP = os.path.join(MODEL_VERSION, 'trained-model/package_to_index_map.json')
