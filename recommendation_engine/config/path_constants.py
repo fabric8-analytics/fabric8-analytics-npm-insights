b27ff90d08fb62fdc59b347b7f73b1e5cd5df9a4
Lucky Suman <lsuman@redhat.com>
2019-11-27 07:54:46 +0530

    Updated paths of files

--- recommendation_engine/config/path_constants.py
+++ recommendation_engine/config/path_constants.py
@@ -1,9 +1,11 @@
 """Contains the path constants for S3 and local storage."""
 import os
 from rudra.data_store.local_data_store import LocalDataStore
+num_users = os.environ.get(num_train)
 
 MODEL_VERSION = os.environ.get('MODEL_VERSION', '2019-01-03')
 TEMPORARY_PATH = '/tmp/trained-model/'
+TEMPORARY_DATA_PATH='/tmp/data/'
 TEMPORARY_MODEL_PATH = '/tmp/intermediate-model/cvae-model/'
 TEMPORARY_DATASTORE = LocalDataStore('/tmp')
 TEMPORARY_LATENT_PATH = os.path.join(TEMPORARY_MODEL_PATH, 'latent_pretrain_all')
@@ -12,9 +14,9 @@ TEMPORARY_CVAE_PATH = os.path.join(TEMPORARY_MODEL_PATH, 'cvae')
 TEMPORARY_PMF_PATH = os.path.join(TEMPORARY_MODEL_PATH, 'pmf-packagedata.mat')
 TEMPORARY_USER_ITEM_FILEPATH = os.path.join(TEMPORARY_PATH, "user_train_data.dat")
 TEMPORARY_ITEM_USER_FILEPATH = os.path.join(TEMPORARY_PATH, "item_train_data.dat")
-USER_ITEM_FILEPATH = os.path.join(MODEL_VERSION, 'trained-model', "user_train_data.dat")
-ITEM_USER_FILEPATH = os.path.join(MODEL_VERSION, 'trained-model', "item_train_data.dat")
-PRECOMPUTED_MANIFEST_PATH = os.path.join(MODEL_VERSION, "trained-model/manifest_user_data.dat")
+USER_ITEM_FILEPATH = os.path.join(MODEL_VERSION, 'data', "")
+ITEM_USER_FILEPATH = os.path.join(MODEL_VERSION, 'data', "item_train_data.dat")
+PRECOMPUTED_MANIFEST_PATH = os.path.join(MODEL_VERSION, "data/manifest_user_data.dat")
 PMF_MODEL_PATH = os.path.join(MODEL_VERSION, 'intermediate-model/cvae-model', 'pmf-packagedata.mat')
 PACKAGE_TO_ID_MAP = os.path.join(MODEL_VERSION, 'trained-model/package_to_index_map.json')
 ID_TO_PACKAGE_MAP = os.path.join(MODEL_VERSION, 'trained-model/index_to_package_map.json')
