c1d496c9fef608a78c4729204929778e4715c490
sawood14012 <msawood@redhat.com>
2021-02-03 13:39:55 +0530

    add test run

--- tests/scripts/entrypoint-test.sh
+++ tests/scripts/entrypoint-test.sh
@@ -20,17 +20,18 @@ export RADONFILESENCODING=UTF-8
 echo "*****************************************"
 echo "*** Cyclomatic complexity measurement ***"
 echo "*****************************************"
-radon cc -s -a -i usr .
+#radon cc -s -a -i usr .
 
 echo "*****************************************"
 echo "*** Maintainability Index measurement ***"
 echo "*****************************************"
-radon mi -s -i usr .
+#radon mi -s -i usr .
 
 echo "*****************************************"
 echo "*** Unit tests ***"
-echo "*****************************************"
-
+echo "*the****************************************"
+pwd
+ls -l
 pytest --cov=/recommendation_engine/ --cov-report=xml --cov-fail-under=$COVERAGE_THRESHOLD -vv /tests/unit_tests/
 
 mv coverage.xml shared
