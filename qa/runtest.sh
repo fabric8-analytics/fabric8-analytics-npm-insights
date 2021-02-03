3699c288aa00d6dec7f78b26dc270fb72db3b51f
sawood14012 <msawood@redhat.com>
2021-02-03 23:12:36 +0530

    stop in gc

--- qa/runtest.sh
+++ qa/runtest.sh
@@ -9,6 +9,7 @@ pushd "${SCRIPT_DIR}/.." > /dev/null
 TEST_IMAGE_NAME='chester-tests'
 
 gc() {
+    docker stop ${TEST_IMAGE_NAME}
     docker rmi -f $(make get-image-name)
     docker rmi -f "${TEST_IMAGE_NAME}"
 }
@@ -17,7 +18,6 @@ if [[ "$CI" -eq "0" ]];
 then
     make docker-build-test
     docker run -v "$PWD:/shared:rw,Z" ${TEST_IMAGE_NAME}
-    docker stop ${TEST_IMAGE_NAME}
     trap gc EXIT SIGINT
 else
     # CI instance will be torn down anyway, don't need to waste time on gc
